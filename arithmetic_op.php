bf8e8957b0bc25078d53e42114b535410d360701
alaminakash810 <alaminakash810@gmail.com>
2024-11-08 15:45:40 +0600

    initital commit and arithmethic op

---
+++ arithmetic_op.php
@@ -0,0 +1,31 @@
+<?php
+
+// Arithmetic operations
+// + - * / ** %
+// increment / decrement
+// ++ , --
+// operator precedence
+// () ** * / % + -
+
+    $x = 10;
+    $y = 2;
+    $z = null;
+
+    //$z = $x + $y;
+    //$z = $x - $y;
+    //$z = $x * $y;
+    //$z = $x / $y;
+    //$z = $x ** $y;
+    $z = $x % $y;
+    //echo $z;
+
+    $counter = 10;
+    //$counter ++; or $counter += 2 or 3 for more increment
+    //$counter --; or $counter -= 2 or 3 for more decrement
+    //echo $counter;
+
+    $total = 1+2-3*4/5**6;
+    echo $total;
+
+
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    echo"I like pizza <br>";
+    echo"It's really good<br>";
+    // this is a comment
+    /* this is a multiline comment */
+    $name = "Akash";
+    echo $name;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button> Order Pizza </button>
+</body>
+</html>
